41a267917009d8e1460b01d425d542d18ddbcc2d
Jack_lin <401788217@qq.com>
2017-03-11 08:50:18 +0800

    create JLUnivalProject

---
+++ JLUnivalProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JLUnivalProject
+//
+//  Created by Sunxiaolin on 17/3/11.
+//  Copyright © 2017年 com.jack.lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
